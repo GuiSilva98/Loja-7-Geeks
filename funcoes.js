f75d6dc1545819c62d0abc555043b025c5c20362
Guilherme da Silva Pereira <62338895+GuiSilva98@users.noreply.github.com>
2020-03-18 10:16:53 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+﻿function voltarHome() {
+    var x;
+    var r = confirm("Tem certeza de voltar?");
+    if (r == true) {
+        x = window.setTimeout(location.href = 'index.html', 0)
+    }
+    else {
+        x = "Você pressionou Cancelar!";
+    } /* htjmtyjtyujty natalia fez essa cagada c: */
+    document.getElementById("demo").innerHTML = x;
+}
